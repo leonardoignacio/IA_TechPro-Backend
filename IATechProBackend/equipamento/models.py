bc9cde8bdf5df4358b6e76af3f51cdd1e4f9b5fd
ANDERSON APARECIDO BOLDINO <anderson.aboldino@senacsp.edu.br>
2025-10-10 11:50:50 -0300

    Criação do app equipamemto domodel

---
+++ IATechProBackend/equipamento/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Equipamento(models.Model):
+    modelo_marca = models.CharField('Modelo e Marca', max_length=50)
+    patrimonio = models.CharField('Patrimonio', max_length=50)
+    num_serie = models.CharField('Número de Série', max_length=50)
+    def __str__(self):
+        return f'{self.modelo_marca} {self.patrimonio} | {self.num_serie}'
+    
