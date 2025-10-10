8b1f8f511f663335851c462c9f5cc9c455595cf8
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2025-10-10 11:34:36 -0300

    Registro de apps Cliente e OrdemdeServico em settings.py

--- IATechProBackend/IATechProBackend/settings.py
+++ IATechProBackend/IATechProBackend/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'core',
+    'Cliente',
+    'OrdemdeServico',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
