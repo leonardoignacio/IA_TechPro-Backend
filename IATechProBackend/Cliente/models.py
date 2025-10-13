be3e8ea27cf6a6badc6b8759b38ac9353d416563
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2025-10-13 09:20:26 -0300

    migration do model Cliente

--- IATechProBackend/Cliente/models.py
+++ IATechProBackend/Cliente/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Cliente(models.Model):
-    ativo = models.CharField('Ativo', max_Length=10)
-    nome = models.CharField('Nome',max_Length=50)
-    cep = models.CharField('CEP', max_Length=10)
+    ativo = models.CharField('Ativo', max_length=10)
+    nome = models.CharField('Nome',max_length=50)
+    cep = models.CharField('CEP', max_length=10)
     data_cadastro = models.DateField(auto_now_add=True)
-    email = models.CharField('E-mail', max_Length=30)
-    telefone = models.CharField('Telefone', max_Length=15)
+    email = models.CharField('E-mail', max_length=30)
+    telefone = models.CharField('Telefone', max_length=15)
     estado = models.CharField('Estado', max_length=2)
-    endereco = models.CharField('Endereço', max_Length=50)
-    cpf_cnpj = models.CharField('CPF/CNPJ', max_Length=16)
-    cidade = models.CharField('Cidade', max_Length=50)
+    endereco = models.CharField('Endereço', max_length=50)
+    cpf_cnpj = models.CharField('CPF/CNPJ', max_length=16)
+    cidade = models.CharField('Cidade', max_length=50)
     def __str__(self):
         return f'{self.nome} {self.email}'
--- IATechProBackend/IATechProBackend/settings.py
+++ IATechProBackend/IATechProBackend/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'Cliente',
     'OrdemdeServico',
     'funcionario',
+    'equipamento',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
