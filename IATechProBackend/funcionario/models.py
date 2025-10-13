1aa49080a14ae1e92f0cf4eebf4d54fa2c9b0de8
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2025-10-13 08:39:43 -0300

    Migrate do sistema de autenticação do django e do model funcionario

--- IATechProBackend/funcionario/models.py
+++ IATechProBackend/funcionario/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Funcionario(models.Model):
-    nome = models.charfield('Nome', max_length=50)
-    cpf = models.charfield('CPF', max_length=15)
-    email = models.charfield('E-mail', max_length=30)
-    telefone = models.charfield('Telefone', max_length=15)
-    especialidade = models.charfield('Especialidade', max_length=50)
+    nome = models.CharField('Nome', max_length=50)
+    cpf = models.CharField('CPF', max_length=15)
+    email = models.CharField('E-mail', max_length=30)
+    telefone = models.CharField('Telefone', max_length=15)
+    especialidade = models.CharField('Especialidade', max_length=50)
     data_admissao = models.DateField(auto_now_add=True)
-    ativo = models.charfield('Ativo', max_length=10)
+    ativo = models.CharField('Ativo', max_length=10)
     salario = models.DecimalField('Salario', max_digits=10, decimal_places=2)
-    cargo = models.charfield('Cargo', max_length=50)
+    cargo = models.CharField('Cargo', max_length=50)
     def __str__(self):
         return f'{self.nome} {self.cargo}'
