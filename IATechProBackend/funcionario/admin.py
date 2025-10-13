bc5fa28024c90fd342e911c80d24a71d727a6d62
ALISON SOUSA DOS SANTOS <alison.ssantos@senacsp.edu.br>
2025-10-13 09:42:32 -0300

    criação do admin.py

--- IATechProBackend/funcionario/admin.py
+++ IATechProBackend/funcionario/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Funcionario
 # Register your models here.
+
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display=('nome', 'cpf', 'email', 'cargo', 'telefone', 'ativo')
+    search_fields=('nome', 'cpf', 'email', 'cargo', 'telefone', 'ativo')
+    list_filter=('nome', 'cpf', 'email', 'cargo', 'telefone', 'ativo')
+
+admin.site.register(Funcionario, FuncionarioAdmin)
