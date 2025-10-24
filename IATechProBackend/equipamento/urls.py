9f1005160004d2645f748a9ad44968d566d28407
MARIA JULIA DORO <maria.jdoro@senacsp.edu.br>
2025-10-24 11:20:12 -0300

    Criação de rotas para Equipamento

--- IATechProBackend/equipamento/urls.py
+++ IATechProBackend/equipamento/urls.py
@@ -3,5 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('registros/', py_obter_Equipamentos, name="equipamento"),
-    path('registros/<int:id_equipamento>', py_obter_Equipamentos, name="equipamento_detalhe")
+    path('registros/<int:id_equipamento>', py_obter_Equipamentos, name="equipamento_detalhe"),
+
+    # Rotas para o CRUD de Equipamentos 
+    path('cria-equipamento', views.py_cria_equipamento, name='cria_equipamento'),
+    path('deleta/<int:id_equipamento>', views.py_deleta_equipamento, name='deleta_equipamento'),
+    path('atualiza/<int:id_equipamento>', views.py_edita_equipamento, name='edita_equipamento'),
 ]
+
