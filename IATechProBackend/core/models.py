66da4b56a71f7a409f9d1cab385db37aaf1a6216
ANDERSON APARECIDO BOLDINO <anderson.aboldino@senacsp.edu.br>
2025-10-31 11:28:49 -0300

    Modificação no Model do Core

--- IATechProBackend/core/models.py
+++ IATechProBackend/core/models.py
@@ -2,5 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    cpf = models.CharField(max_length=200, unique=True) 
-    endereco = models.CharField(max_length=200)
+    ativo = models.BooleanField(default=True)
+    nome = models.CharField('Nome', max_length=50)
+    cpf_cnpj = models.CharField(max_length=200, unique=True) 
+    email = models.EmailField('E-mail', max_length=30, unique=True)
+    telefone = models.CharField('Telefone', max_length=15)
+    logradouro = models.CharField('Logradouro',max_length=200)
+    cep = models.CharField('CEP', max_length=10)
+    cidade = models.CharField('Cidade', max_length=50)
+    estado = models.CharField('Estado', max_length=2)
+    
--- IATechProBackend/core/serializers.py
+++ IATechProBackend/core/serializers.py
@@ -5,7 +5,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'cpf', 'username', 'first_name', 'email')
+        fields = ('id', '', 'username', 'first_name', 'email')
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
@@ -16,3 +16,6 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+
+    # "cpf:"", "username:"", "password:"", "first_name:"#, "email:"""
