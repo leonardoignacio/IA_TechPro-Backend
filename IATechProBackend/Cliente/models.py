1beb8aa51a2b16650fc234efb5a2178008629ce1
ALISON SOUSA DOS SANTOS <alison.ssantos@senacsp.edu.br>
2025-10-31 11:25:41 -0300

    Modificação nos models das tabelas funcionario e cliente

--- IATechProBackend/Cliente/models.py
+++ IATechProBackend/Cliente/models.py
@@ -1,16 +1,11 @@
 from django.db import models
+from core.models import models
 
 # Create your models here.
 class Cliente(models.Model):
-    ativo = models.BooleanField(default=True)
-    nome = models.CharField('Nome',max_length=50)
-    cep = models.CharField('CEP', max_length=10)
     data_cadastro = models.DateField(auto_now_add=True)
-    email = models.EmailField('E-mail', max_length=30, unique=True)
-    telefone = models.CharField('Telefone', max_length=15)
-    estado = models.CharField('Estado', max_length=2)
-    endereco = models.CharField('Endereço', max_length=50)
-    cpf_cnpj = models.CharField('CPF/CNPJ', max_length=16)
-    cidade = models.CharField('Cidade', max_length=50)
+    keyuser = models.CharField('Keyuser', max_length=50)
+    impresa_setor = models.CharField('Cargo', max_length=50)
+
     def __str__(self):
         return f'{self.nome} {self.email}'
