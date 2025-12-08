468fba8ab118f85dd4032388ae5748adc82fb22b
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2025-12-08 10:52:44 -0300

    configurações para rodar em produção

--- IATechProBackend/IATechProBackend/settings.py
+++ IATechProBackend/IATechProBackend/settings.py
@@ -18,9 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'IATechProBackend.onrender.com', 'onrender.com', '34.83.64.96']
 
 
 # Application definition
